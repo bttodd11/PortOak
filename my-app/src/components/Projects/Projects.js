f97ee0bad624e5a64ab2db71addffe40245ae940
Brian <BTodd@Brians-MacBook-Pro.local>
2020-11-02 01:28:59 -0600

    Stopping point   started on project section

--- my-app/src/components/Projects/Projects.js
+++ my-app/src/components/Projects/Projects.js
@@ -1,55 +1,44 @@
 import React from "react";
-import { Container, Col, Row} from "react-bootstrap";
+import { Container, Col, Row, Card, CardDeck, ListGroup} from "react-bootstrap";
 import './Projects.css';
+import SkillsFund from './img/sfIcon.png';
+import Chrip from './img/batIcon.png';
+import Chosen from './img/chosenIcon.png';
 
 const Projects = () =>{
   return (
-    <div>
+<div>
       <h2 class="projectTitle">
         Projects
       </h2>
-      <Container fluid>
-        <Row>
-          <Col>
-            <h6>Skills Fund Mock Up</h6>
-            <ul>
-              <li>jQuery</li>
-              <li>UiKit</li>
-              <li>Bootstrap</li>
-            </ul>
-          </Col>
-          <Col>
-            <h6>MLB Ballot</h6>
-            <ul>
-              <li>PHP</li>
-              <li>MySQL</li>
-              <li>Bootstrap</li>
-            </ul>
-          </Col>
-          <Col>
-            <h6>EricTheChosen</h6>
-            <ul>
-              <li>React</li>
-              <li>JavaScript</li>
-              <li>Sass</li>
-            </ul>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-              <h6>Marvel Database</h6>
-              <li>JSON ( API Data )</li>
-              <li>React</li>
-              <li>JavaScript</li>
-          </Col>
-          <Col>
-              <h6>Chiroptophobis</h6>
-              <li>JavaScript</li>
-              <li>CSS</li>
-
-          </Col>
-        </Row>
-      </Container>
+<CardDeck>
+      <Card style={{ width: '8rem'}} >
+    <Card.Img variant="top" className="icons" src={SkillsFund} />
+      <Card.Header className="listTitle">Skills Fund Mock Site</Card.Header>
+      <ListGroup className="listCenter" variant="flush">
+    <ListGroup.Item>Bootstrap</ListGroup.Item>
+    <ListGroup.Item>UiKit</ListGroup.Item>
+    <ListGroup.Item>jQuery</ListGroup.Item>
+  </ListGroup>
+  </Card>
+  <Card style={{ width: '8rem'}} >
+    <Card.Img variant="top" className="icons" src={Chosen} />
+      <Card.Header className="listTitle">EricTheChosen</Card.Header>
+      <ListGroup className="listCenter" variant="flush">
+    <ListGroup.Item>Bootstrap</ListGroup.Item>
+    <ListGroup.Item>Sass</ListGroup.Item>
+    <ListGroup.Item>JavaScript</ListGroup.Item>
+  </ListGroup>
+  </Card>
+  <Card style={{ width: '8rem'}} >
+    <Card.Img variant="top" className="icons" src={Chrip} />
+      <Card.Header className="listTitle">Skills Fund Mock Site</Card.Header>
+      <ListGroup className="listCenter" variant="flush">
+    <ListGroup.Item>Less</ListGroup.Item>
+    <ListGroup.Item>JavaScript</ListGroup.Item>
+  </ListGroup>
+  </Card>
+</CardDeck>
     </div>
   )
 }
